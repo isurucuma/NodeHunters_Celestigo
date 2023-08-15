7a2110deda66e92c8ade3f61b06e2f1874c76e8f
Madushan Gunarathne <madushangunarathne@gmail.com>
2023-08-15 20:33:56 +0530

    Created few screens

---
+++ frontend/src/app/welcome/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import AppTemplate from "@/components/templates/AppTemplate";
+import PageTitle from "@/components/atoms/PageTitle/PageTitle";
+import Box from "@mui/material/Box";
+import TextBlock from "@/components/atoms/TextBlock/TextBlock";
+import PrimaryButton from "@/components/atoms/PrimaryButton/PrimaryButton";
+
+const Welcome = () => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <Box sx={{ mb: 2 }}>
+        <PageTitle title="Welcome" />
+      </Box>
+      <Box sx={{ mb: 7 }}>
+        <TextBlock content="Get ready to launch into a new era of exploration and adventure. Your interstellar journey begins now. Let's explore the cosmos together!" />
+      </Box>
+      <Box>
+        <PrimaryButton name="Let's go" />
+      </Box>
+    </Box>
+  );
+};
+
+export default Welcome;
---
+++ frontend/src/components/atoms/TextBlock/TextBlockStyles.ts
@@ -0,0 +1,14 @@
+const textBlockStyles = {
+  root: {
+    color: "#C9C9C9",
+    textAlign: "center",
+    textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    fontFamily: "Exo",
+    fontSize: "16px",
+    fontStyle: "normal",
+    fontWeight: 300,
+    lineHeight: "normal",
+  },
+};
+
+export default textBlockStyles;
