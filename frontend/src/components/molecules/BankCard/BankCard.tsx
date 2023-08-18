40365fcab86e11c8094b0763e9e7cd2d39b2383a
Madushan Gunarathne <madushangunarathne@gmail.com>
2023-08-18 14:00:37 +0530

    Bank card pages created

--- frontend/src/components/molecules/BankCard/BankCard.tsx
+++ frontend/src/components/molecules/BankCard/BankCard.tsx
@@ -1,21 +1,36 @@
 import BankCardDate from "@/components/atoms/BankCardDate/BankCardDate";
+import BankCardName from "@/components/atoms/BankCardName/BankCardName";
 import BankCardNum from "@/components/atoms/BankCardNum/BankCardNum";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import React from "react";
 
 const BankCard = ({
+  name,
   digits,
   date,
   type,
+  isDefault = false,
 }: {
+  name: string;
   digits: string;
   date: string;
   type: string;
+  isDefault?: boolean;
 }) => {
   return (
-    <Box sx={{ mb: "20px", position: "relative", width: "90%" }}>
-      <Box sx={{ borderBottom: "1px solid #B0BAC3", padding: "0 8px" }}>
+    <Box
+      sx={{
+        mb: "20px",
+        position: "relative",
+        width: "90%",
+        borderRadius: "17px",
+        border: "2px solid #56BFC7",
+        background: "rgba(255, 255, 255, 0.00)",
+        boxShadow: "0px 3px 8px 7px rgba(109, 0, 161, 0.47)",
+      }}
+    >
+      <Box>
         <Box
           component="img"
           sx={{
@@ -32,25 +47,41 @@ const BankCard = ({
           alt={""}
         />
       </Box>
-      <Grid
-        container
-        spacing={2}
+
+      <Box
         sx={{
           position: "absolute",
-          bottom: "5px",
-          left: "0",
-          width: "95%",
+          top: "70px",
+          left: "38px",
           margin: "auto",
         }}
       >
-        <Grid item xs={8}>
-          {" "}
-          <BankCardNum digits={digits} />
-        </Grid>
-        <Grid item xs={4}>
-          <BankCardDate date={date} />
-        </Grid>
-      </Grid>
+        <BankCardNum digits={digits} />
+      </Box>
+
+      <Box
+        sx={{
+          position: "absolute",
+          top: "120px",
+          left: "40px",
+          margin: "auto",
+        }}
+      >
+        {" "}
+        <BankCardDate date={date} />
+      </Box>
+
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: "25px",
+          left: "40px",
+          margin: "auto",
+        }}
+      >
+        {" "}
+        <BankCardName name={name} />
+      </Box>
     </Box>
   );
 };
